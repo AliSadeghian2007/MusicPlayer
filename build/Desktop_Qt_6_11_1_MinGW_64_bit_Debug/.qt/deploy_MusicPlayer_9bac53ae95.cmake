14726794b87782bd8db19a33fa9166430a1bd360
AliSadeghian2007 <amiras8416@gmail.com>
2026-07-11 16:41:22 +0330

    Initial project structure

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/deploy_MusicPlayer_9bac53ae95.cmake
@@ -0,0 +1,8 @@
+include("F:/Project/C++/Project/MusicPlayer/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/QtDeploySupport.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/MusicPlayer-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE "F:/Project/C++/Project/MusicPlayer/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/MusicPlayer.exe"
+    GENERATE_QT_CONF
+)
